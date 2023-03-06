c9f1879fe62caa9e69d693314dc4dbc9ee013d1d
Daiki Anjiki <daikianjiki@gmail.com>
2023-03-06 12:39:32 -0800

    ignore files .idea/ and target

---
+++ src/main/java/com/revature/com/Driver.java
@@ -0,0 +1,13 @@
+package com.revature.com;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        // check the length
+
+        if (args.length > 0)
+            System.out.println("Hello world from " + args[0]);
+        else
+            System.out.println("Hello world.");
+    }
+}
